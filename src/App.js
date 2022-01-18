03221a568f3588891c31d807e2012c7442489ca1
angelina <tuyentr15@gmail.com>
2022-01-17 16:06:32 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import food from "./img/food.jpeg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-pink-300">
+      <div>
+        <nav>
+          <div>
+            <h1>
+              <a href="/">Food Ninja</a>
+            </h1>
+          </div>
+          <ul>
+            <li>
+              <a href="#">
+                <span className="text-xs">Home</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span>About</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span>Contact</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <main>
+        <div>
+          <a href="#">Log in</a>
+          <a href="#">Sign up</a>
+        </div>
+
+        <header>
+          <h2 className="text-bold uppercase font-xl bg-gray-400 p-2 ">
+            Recipes
+          </h2>
+          <h3 className="bg-red-600 m-4 border-8 border-yellow-800">
+            For Ninjas
+          </h3>
+        </header>
+
+        <div>
+          <h4>Latest Recipes</h4>
+          <div>
+            <div>
+              <img src={food} alt="food" />
+
+              <span> 5 Bean Chilli Stew</span>
+              <span> Recipes by Mario</span>
+            </div>
+          </div>
+        </div>
+
+        <h4>Most Popular</h4>
+        <div></div>
+        <div>
+          <div>Load More</div>
+        </div>
+      </main>
     </div>
   );
 }
